4fb22aff6b8352a0e655f7345a5119f16e4aecc1
Code-lab-dev <fawassaka862@gmail.com>
2024-11-28 18:00:47 -0500

    Update WeatherCard.tsx

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -14,7 +14,7 @@ const WeatherCard = ({ weather }: WeatherCardProps) => {
       exit={{ y: -20, opacity: 0 }}
       transition={{ duration: 0.3 }}
       className={`flex justify-center items-center rounded-3xl backdrop-blur-md bg-white/10 shadow-lg border border-white/20 
-  w-full max-w-[90vw] md:max-w-[70vw] lg:w-[100vh] h-[50vh] -ml-0 md:-ml-30 lg:-ml-60`}
+  w-full max-w-[90vw] md:max-w-[70vw] lg:w-[100vh] h-[50vh] -ml-0 md:-ml-30 lg:-ml-60
 
         ${weather.condition === 'sunny' ? 'bg-sunny/10' :
           weather.condition === 'rainy' ? 'bg-rainy/10' :
